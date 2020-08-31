bc864cee232e2c2d06abda70b5bbd4c8729853e1
Aaron Lin <aaronlin256@gmail.com>
2020-08-30 22:50:31 -0400

    feat(firestore): ability to add vin document to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'HomePage.dart';
+import 'pages/HomePage.dart';
 
-void main() => runApp(MyApp());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+} 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
